622df91bbd20e65b15ae439845a13c2829314d14
hickeyli <hickeyli@msu.edu>
2024-11-11 10:41:56 -0500

    Fixed some spawn issues

--- client/common.lua
+++ client/common.lua
@@ -1,95 +1,51 @@
--- function as existing in original R* scripts
-local function freezePlayer(id, freeze)
-    local player = id
-    SetPlayerControl(player, not freeze, false)
-
-    local ped = GetPlayerPed(player)
-
-    if not freeze then
-        if not IsEntityVisible(ped) then
-            SetEntityVisible(ped, true)
-        end
-
-        if not IsPedInAnyVehicle(ped) then
-            SetEntityCollision(ped, true)
-        end
-
-        FreezeEntityPosition(ped, false)
-        --SetCharNeverTargetted(ped, false)
-        SetPlayerInvincible(player, false)
-    else
-        if IsEntityVisible(ped) then
-            SetEntityVisible(ped, false)
-        end
-
-        SetEntityCollision(ped, false)
-        FreezeEntityPosition(ped, true)
-        --SetCharNeverTargetted(ped, true)
-        SetPlayerInvincible(player, true)
-        --RemovePtfxFromPed(ped)
-
-        if not IsPedFatallyInjured(ped) then
-            ClearPedTasksImmediately(ped)
-        end
-    end
-end
+local isSpawning = false
 
 RegisterNetEvent("desync-spawnmanager:SpawnCharacter")
 AddEventHandler("desync-spawnmanager:SpawnCharacter", function(coords)
+    -- Prevent multiple spawns
+    if isSpawning then return end
+    isSpawning = true
+    
     DoScreenFadeOut(500)
-
-    while not IsScreenFadedOut() do
-        Citizen.Wait(0)
-    end
-
-    freezePlayer(PlayerId(), true)
-
-    -- preload collisions for the spawnpoint
-    RequestCollisionAtCoord(coords.x, coords.y, coords.z)
-
-    -- spawn the player
+    while not IsScreenFadedOut() do Wait(0) end
+    
     local ped = PlayerPedId()
-
-    -- V requires setting coords as well
-    SetEntityCoordsNoOffset(ped, coords.x, coords.y, coords.z, false, false, false, true)
-
-    NetworkResurrectLocalPlayer(coords.x, coords.y, coords.z, coords.w, true, true, false)
-
-    -- gamelogic-style cleanup stuff
+    
+    -- Freeze player during spawn
+    FreezeEntityPosition(ped, true)
+    SetEntityVisible(ped, false)
+    
+    -- Request collision and wait for it to load
+    RequestCollisionAtCoord(coords.x, coords.y, coords.z)
+    
+    -- Set exact coordinates
+    SetEntityCoordsNoOffset(ped, coords.x, coords.y, coords.z, false, false, false)
+    SetEntityHeading(ped, coords.heading)
+    
+    -- Network resurrect to ensure proper spawn
+    NetworkResurrectLocalPlayer(coords.x, coords.y, coords.z, coords.heading, true, true)
+    
+    -- Clear any existing tasks
     ClearPedTasksImmediately(ped)
-    --SetEntityHealth(ped, 300) -- TODO: allow configuration of this?
-    RemoveAllPedWeapons(ped) -- TODO: make configurable (V behavior?)
+    RemoveAllPedWeapons(ped)
     ClearPlayerWantedLevel(PlayerId())
-
-    -- why is this even a flag?
-    --SetCharWillFlyThroughWindscreen(ped, false)
-
-    -- set primary camera heading
-    --SetGameCamHeading(spawn.heading)
-    --CamRestoreJumpcut(GetGameCam())
-
-    -- load the scene; streaming expects us to do it
-    --ForceLoadingScreen(true)
-    --loadScene(spawn.x, spawn.y, spawn.z)
-    --ForceLoadingScreen(false)
-
-    local time = GetGameTimer()
-
-    while (not HasCollisionLoadedAroundEntity(ped) and (GetGameTimer() - time) < 5000) do
-        Citizen.Wait(0)
+    
+    -- Wait for collision to load
+    while not HasCollisionLoadedAroundEntity(ped) do
+        Wait(0)
     end
-
-    ShutdownLoadingScreenNui()
-
-    if IsScreenFadedOut() then
-        DoScreenFadeIn(500)
-
-        while not IsScreenFadedIn() do
-            Citizen.Wait(0)
-        end
-    end
-
-    -- and unfreeze the player
-    freezePlayer(PlayerId(), false)
+    
+    -- Unfreeze and show player
+    FreezeEntityPosition(ped, false)
+    SetEntityVisible(ped, true)
+    
+    -- Fade back in
+    DoScreenFadeIn(500)
+    while not IsScreenFadedIn() do Wait(0) end
+    
+    -- Reset spawn lock
+    Wait(1000) -- Wait a bit before allowing another spawn
+    isSpawning = false
 end)
+
 
--- server/common.lua
+++ server/common.lua
@@ -1,13 +1,13 @@
 -- Triggered when a character should spawn
 RegisterNetEvent("desync-spawnmanager:SpawnCharacter")
 AddEventHandler("desync-spawnmanager:SpawnCharacter", function(netId, coords)
-    SpawnCharacter(netId, coords)
-    -- TriggerEvent("desync-core-rp:CharacterSelected2", netId, 'char1') -- temporary
+    -- Store the spawn coordinates server-side to prevent overrides
+    local spawnCoords = {
+        x = coords.x,
+        y = coords.y,
+        z = coords.z,
+        heading = coords.heading
+    }
+    
+    TriggerClientEvent("desync-spawnmanager:SpawnCharacter", netId, spawnCoords)
 end)
-
-function SpawnCharacter(netId, coords)
-    print("Spawning character at: " .. coords.x .. ", " .. coords.y .. ", " .. coords.z)
-    -- print("Triggering client event SpawnCharacter")
-
-    TriggerClientEvent("desync-spawnmanager:SpawnCharacter", netId, coords)
-end
